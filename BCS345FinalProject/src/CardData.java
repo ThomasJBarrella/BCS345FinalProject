177e59823cd47e1c067791d65eb236598b63a570
KyyPie <KyyCowPig@gmail.com>
2019-12-02 00:44:57 -0500

    Displaying 4 cards

--- BCS345FinalProject/src/CardData.java
+++ BCS345FinalProject/src/CardData.java
@@ -1,7 +1,11 @@
 import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class CardData {
-	public void getCards() {
+	Image finalCard[] = new Image[4]; //making the image and imageview public so we can add them to the group later
+	ImageView finalImage[] = new ImageView[4];
+	public void getCards() {	
 	int suit[] = new int[3];
 	int cards[] = new int[12];
 	//when searching for the cards, put together the string of numbers that are suit first, then cards
@@ -15,7 +19,7 @@ public class CardData {
 		cards[j] = j;
 	}
 	//create the string that will be used to match the card number in the zipped file
-	String findCard = " ";
+	String findCard = "";
 	int rnd = new Random().nextInt(suit.length);
 	rnd = suit[rnd];
 	findCard = findCard + rnd;
@@ -23,6 +27,12 @@ public class CardData {
 	rnd2 = cards[rnd2];
 	findCard = findCard + rnd2;
 	//based on findCard, search the file and display the necessary card in the proper location
-	}	
+	for(int i = 0; i<finalCard.length; i++) { 
+	finalCard[i] = new Image(getClass().getResourceAsStream("/res/"+findCard+".png")); //This will create 1 random card based on findCard into an image
+	finalImage[i] = new ImageView(finalCard[i]); //adding that image to a view
+	finalImage[i].setY(200); //setting the initial y, note that all the iterations of this array are gonna be set at the same y allowing for them to
+							 //line up horizontally.
+	}
+}
 }
 
